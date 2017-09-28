5d42b68fd20812f1dbf834ee1a506496d0064464
DesC <cahill.desmond@gmail.com>
2017-09-28 13:22:02 +0000

    ac create complete

---
+++ js/app.js
@@ -0,0 +1,18 @@
+angular.module('TodoApp',['ngRoute', 'RouteControllers', 'UserService', 'angular-storage', "TodoService"]);
+angular.module('TodoApp').config(function
+    ($locationProvider, $routeProvider) {
+        $locationProvider.html5Mode(true);
+        
+        $routeProvider.when("/", {
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController'
+        })
+        .when("/todo", {
+            templateUrl: 'templates/todo.html',
+            controller: 'TodoController'
+        })
+        .when('/accounts/register', {
+            templateUrl: 'templates/register.html',
+            controller: 'RegisterController'
+        });
+});
---
+++ js/service.js
@@ -0,0 +1,26 @@
+//new angular module with name and has no dependancies
+angular.module("UserService", [])
+//create factory with name and function dependant on andgular http lib
+    .factory("UserAPIService", function($http) {
+        UserAPIService = {
+            callAPI: function(url, data) {
+                return $http.post(url, data);
+            }
+        };
+        return UserAPIService;
+    });
+    
+angular.module("TodoService", [])
+    .factory("TodoAPIService", function($http) {
+        TodoAPIService = {
+            getTodos: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                   return $http.get(url, {params:{"username": data}}, header);
+                },
+            createTodo: function(url, data, token) {
+                var header = "Authorization: JWT " + token;
+                return $http.post(url, data, token);
+                }
+            };
+             return TodoAPIService;
+    })
